7fbf813b661103951e1a4277eb8547324a1d6223
talles <talles_brendo@hotmail.com>
2024-01-19 23:54:32 -0300

    teste

---
+++ curso-angular/src/app/components/teste-component/teste-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste-component',
+  templateUrl: './teste-component.component.html',
+  styleUrl: './teste-component.component.scss'
+})
+export class TesteComponentComponent {
+
+}
